d53347b44e8ff48fd5879f6bf4670f8a47d9ea68
yogeshsindagi <153347483+yogeshsindagi@users.noreply.github.com>
2025-06-19 20:40:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-DB_SERVER = ""
-DB_NAME = "your"
+DB_SERVER = "your_server.database.windows.net"
+DB_NAME = "your_db_name"
 DB_USER = "your_db_username"
-DB_PASSWORD = "yout_password"
+DB_PASSWORD = "your_db_password"
 
 EMAIL_SENDER = "your_mail_id"
 EMAIL_PASSWORD = "your_password"
